b50c00f85838cce2ec38c778d73be0f409cb9c11
Alexander Forslund <alexforslund.dev@gmail.com>
2022-11-12 16:28:48 +0100

    Change schtuff
    Co-authored-by: Carlltz <Carlltz@users.noreply.github.com>

--- src/main/java/dev/forslund/mc/commands/CMDInterpreter.java
+++ src/main/java/dev/forslund/mc/commands/CMDInterpreter.java
@@ -1,12 +1,20 @@
 package dev.forslund.mc.commands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
+import org.bukkit.block.Lectern;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ThrowableProjectile;
+import org.bukkit.entity.ThrownExpBottle;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Score;
@@ -51,57 +59,60 @@ public class CMDInterpreter implements CommandExecutor {
     }
 
     private void interpret() {
-        String[] r_type = { "add", "sub", "set", "jumpCon" }; // 3 bitar o, 2 bitar $rt, 2 bitar $rs, 1 bit imm // set
-                                                              // seperate type?? Then: 3 bit o, 2 bit $rt, 3 bit imm
-        String[] j_type = { "jump" }; // 3 bitar o, 5 bitar imm
-        String[] spec_type = { "input", "print", "exit" }; // (I förekommande ordning) 3 bitar o, 2 bitar $rt, 3 bitar
+        String[] r_type = { "add", "jumpCon" };  // 3 bitar o, 2 bitar $rt, 2 bitar $rs, 1 bit imm (add imm = sign 0=(+) 1=(-))
+        String[] j_type = { "jump" };   // 3 bitar o, 5 bitar imm
+        String[] spec_type = {"input", "print", "set", "exit", "addi" }; // (I förekommande ordning) 3 bitar o, 2 bitar $rt, 3 bitar
                                                            // bajs | 3 bitar o, 5 bitar imm eller 3 bitar o, 2 bitar
-                                                           // $rt, resterande bajs | 3 bitar o, resterande bajs
+                                                           // $rt, resterande bajs | 3 bitar, 2 bitar rt, 3 bitar imm | 3 bitar o, resterande bajs
 
         FileConfiguration config = p.getConfig();
 
         chest = (Chest) p.getServer().getWorld("World").getBlockAt(0, 56, 0).getState();
         ItemStack[] itemArray = chest.getInventory().getContents();
 
-        int i = 0;
+        int square = 0;
         try {
-            while (i < itemArray.length - 1) { // Tobias dont judge the if statement för switch funkade inte for some
+            while (square < itemArray.length - 1) { // Tobias dont judge the if statement för switch funkade inte for some
                                                // anledning
-                if (itemArray[i] == null) {
-                    i++;
+                if (itemArray[square] == null) {
+                    square++;
                     continue;
                 }
-                if (itemArray[i].getType().equals(Material.COMPARATOR)) {
+                if (itemArray[square].getType().equals(Material.COMPARATOR)) {
                     // jumCon(itemArray[i+1], itemArray[i+2], itemArray[i+3]); // rt, rs, imm
-                    i += 4;
-                } else if (itemArray[i].getType().equals(Material.FERN)) {
-                    add(itemArray[i + 1], itemArray[i + 2], itemArray[i + 3]); // rt, rs, imm
-                    i += 4;
-                } else if (itemArray[i].getType().equals(Material.IRON_NUGGET)) {
-                    sub(itemArray[i + 1], itemArray[i + 2], itemArray[i + 3]); // rt, rs, imm
-                    i += 4;
-                } else if (itemArray[i].getType().equals(Material.MAGENTA_GLAZED_TERRACOTTA)) {
-                    // jump(itemArray[i+1]); // imm
-                    i += 2;
-                } else if (itemArray[i].getType().equals(Material.ARROW)) {
-                    // set(itemArray[i+1], itemArray[i+2]); // rt, imm
-                    i += 3;
-                } else {
-                    i++; // Fix for temp bug where if you put a lone paper it will infinite loop
+                    square += 4;
+                } else if (itemArray[square].getType().equals(Material.FERN)) {
+                    add(itemArray[square + 1], itemArray[square + 2], itemArray[square + 3]); // rt, rs, imm
+                    square += 4;
+                } else if (itemArray[square].getType().equals(Material.IRON_NUGGET)) {
+                    sub(itemArray[square + 1], itemArray[square + 2], itemArray[square + 3]); // rt, rs, imm
+                    square += 4;
+                } else if (itemArray[square].getType().equals(Material.MAGENTA_GLAZED_TERRACOTTA)) {
+                    square = jump(itemArray[square+1], square); // imm
+                } else if (itemArray[square].getType().equals(Material.ARROW)) {
+                    set(itemArray[square+1], itemArray[square+2]); // rt, imm
+                    square += 3;
+                } else if (itemArray[square].getType().equals(Material.NAME_TAG)){
+                    input(itemArray[square+1]);
+                    square += 2;
+                }
+                
+                else {
+                    square++; // Fix for temp bug where if you put a lone paper it will infinite loop
                 }
             }
         } catch (Exception e) {
             p.getServer().broadcastMessage(ChatColor.RED + "Ouef. (Invalid input)");
+            e.printStackTrace();
         }
     }
 
-    private void add(ItemStack rt, ItemStack rs, ItemStack imm) { // rt = rt + rs + imm (o = 3 bits, rt = 2 bits, rs = 2
-                                                                  // bits, imm = 1 bit)
+    private void add(ItemStack rt, ItemStack rs, ItemStack imm) { // rt = rt + rs + imm (o = 3 bits, rt = 2 bits, rs = 2 bits, imm = 1 bit) (r-type)
         Score rtScore = getScore(rt);
-        if (rtScore.equals(scores.get(0))) { // Check if rt (first arg) == $0, which is NOT allowed.
-            p.getServer().broadcastMessage(ChatColor.RED + "Cannot add to $0.");
+        if (isZeroRegister(rtScore)) {
             return;
         }
+        
         Score rsScore = getScore(rs);
         int immVal = Integer.parseInt(imm.getItemMeta().getDisplayName());
         if (immVal != 0 && immVal != 1) { // Only 1 bit sadly ):
@@ -113,13 +124,12 @@ public class CMDInterpreter implements CommandExecutor {
         rtScore.setScore(rtScore.getScore() + rsScore.getScore() + immVal);
     }
 
-    private void sub(ItemStack rt, ItemStack rs, ItemStack imm) { // rt = rt - rs - imm (o = 3 bits, rt = 2 bits, rs = 2
-                                                                  // bits, imm = 1 bit)
+    private void sub(ItemStack rt, ItemStack rs, ItemStack imm) { // rt = rt - rs - imm (o = 3 bits, rt = 2 bits, rs = 2 bits, imm = 1 bit) (r-type)
         Score rtScore = getScore(rt);
-        if (rtScore.equals(scores.get(0))) { // Check if rt (first arg) == $0, which is NOT allowed.
-            p.getServer().broadcastMessage(ChatColor.RED + "Cannot subtract from $0.");
+        if (isZeroRegister(rtScore)) {
             return;
         }
+
         Score rsScore = getScore(rs);
         int immVal = Integer.parseInt(imm.getItemMeta().getDisplayName());
         if (immVal != 0 && immVal != 1) { // Only 1 bit sadly ):
@@ -131,21 +141,38 @@ public class CMDInterpreter implements CommandExecutor {
         rtScore.setScore(rtScore.getScore() - rsScore.getScore() - immVal);
     }
 
-    private void set(ItemStack rt, ItemStack imm) { // rt = imm (o = 3 bits, rt = 2 bits, imm = 3 bits)
+    private int jump(ItemStack imm, int square) {
+        int immValue = Integer.parseInt(imm.getItemMeta().getDisplayName());
+        return square - immValue;
+    }
+
+    private void set(ItemStack rt, ItemStack imm) { // rt = imm (o = 3 bits, rt = 2 bits, imm = 3 bits) (special-type)
         Score rtScore = getScore(rt);
-        if (rtScore.equals(scores.get(0))) { // Check if rt (first arg) == $0, which is NOT allowed.
-            p.getServer().broadcastMessage(ChatColor.RED + "Cannot add to $0.");
+        if (isZeroRegister(rtScore)) {
             return;
         }
 
         int immVal = Integer.parseInt(imm.getItemMeta().getDisplayName());
-        if (immVal < 0 && 7 < immVal) { // Only 3 bit happy :)
+        if (immVal < 0 || 7 < immVal) { // Only 3 bit happy :)
             p.getServer().broadcastMessage(ChatColor.RED + "Bad immediate value! Maximum 3 bit (0-7)");
             return;
         }
         // dont need to check if > or < int32 max/min because java is default int32
         // it will throw an error anyway
         rtScore.setScore(immVal);
+    }
+
+    private void input(ItemStack rt) { // 3 bits O, 2 bits $rt, resten bajs
+        Score rtScore = getScore(rt);
+        if (isZeroRegister(rtScore)) {
+            return;
+        }
+        chest.getLocation().add(0, -2, -2).getBlock().setType(Material.REDSTONE_BLOCK);
+        Lectern lectern = (Lectern) chest.getLocation().add(-2, 0, 0).getBlock().getState();
+
+        // Spawn effect
+        Location l = lectern.getLocation();
+        p.getServer().getWorld("world").playEffect(l, Effect.END_GATEWAY_SPAWN, 1000);
     }
 
     private Score getScore(ItemStack rt) {
@@ -159,9 +186,17 @@ public class CMDInterpreter implements CommandExecutor {
         } else if (rt.getType().equals(Material.NETHERITE_BLOCK)) {
             return scores.get(0); // $0
         } else {
-            p.getServer().broadcastMessage("bad bad");
             return null; // Bad code but whatever.
         }
+    }
+
+    private boolean isZeroRegister(Score rtScore) {
+        if (rtScore.equals(scores.get(0))) { // Check if rt (first arg) == $0, which is NOT allowed.
+            p.getServer().broadcastMessage(ChatColor.RED + "Cannot set $0.");
+            return true;
+        }
+        return false;
+
     }
 }
 
